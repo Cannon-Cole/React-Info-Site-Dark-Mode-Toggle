c3eb337d24aeb946ad13e2bbe2f16c35c02b2ac6
Cannon-Cole <colecannonschool@gmail.com>
2022-05-19 18:31:28 -0600

    Project complete on commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
+  const [dark, setDark] = React.useState(false);
+  function toggleDark() {
+    setDark((dark) => !dark);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar darkMode={dark} toggleDarkMode={toggleDark} />
+      <Main darkMode={dark} />
     </div>
   );
 }
